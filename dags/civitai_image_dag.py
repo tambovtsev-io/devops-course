76b6a4c939a4adc3aa70e1e0b60c2b996ed74ccc
Ilia Tambovtsev <tambovtsev.io@phystech.edu>
2025-02-28 14:41:31 +0300

    feat: configure dag

--- dags/civitai_image_dag.py
+++ dags/civitai_image_dag.py
@@ -38,7 +38,7 @@ def fetch_images_task(**context):
     response = asyncio.run(
         fetch_images(
             limit=context["params"].get("limit", 100),
-            sort=SortType(context["params"].get("sort", SortType.NEWEST.value)),
+            sort=SortType(context["params"].get("sort", SortType.MOST_REACTIONS.value)),
         )
     )
     context["task_instance"].xcom_push(key="image_response", value=response)
@@ -52,7 +52,6 @@ def load_data_task(**context):
     if not image_response:
         raise ValueError("No data received from fetch task")
 
-    # response = ImageResponse(**image_response)
     response = image_response
     db = Database()
     db.save_image_data(response)
@@ -61,12 +60,17 @@ def load_data_task(**context):
 with DAG(
     "civitai_etl",
     description="Fetch and load CivitAI data",
-    schedule_interval=timedelta(hours=4),
+    schedule_interval=timedelta(hours=12),
     start_date=datetime(2024, 1, 1),
     catchup=False,
     params=dict(
         limit=100,
-        sort=SortType.NEWEST.value,
+        sort=SortType.MOST_REACTIONS.value,
+    ),
+    default_args=dict(
+        owner="Ilia",
+        retries=3,  # 30 minutes gap to perform the task
+        retry_delay=timedelta(minutes=10),
     ),
 ) as dag:
     fetch_data = PythonOperator(
